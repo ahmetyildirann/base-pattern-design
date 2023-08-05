ce00b3bc776c8ee847428a515a1649d43cf7f018
ahmetyildirann <56588376+ahmetyildirann@users.noreply.github.com>
2023-08-05 13:17:25 +0300

    adding Test

---
+++ src/test/java/com/bootcamp/basePatternDesign/TestTest.java
@@ -0,0 +1,40 @@
+package com.bootcamp.basePatternDesign;
+
+import org.aspectj.lang.annotation.After;
+import org.junit.jupiter.api.*;
+
+public class TestTest {
+
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("BeforeAll");
+    }
+    @AfterAll
+    static void afterAll() {
+        System.out.println("AfterAll");
+    }
+
+    @BeforeEach
+    void beforeEach() {
+        System.out.println("BeforeEach");
+    }
+
+    @AfterEach
+    void afterEach() {
+        System.out.println("AfterEach");
+    }
+    @Test
+    void test() {
+        System.out.println("Test");
+    }
+
+    @Test
+    void test1() {
+        System.out.println("Test1");
+    }
+
+    @Test
+    void test2() {
+        System.out.println("Test2");
+    }
+}
